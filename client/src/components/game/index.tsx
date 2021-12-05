a3d265691612d341db3a87bf53bf59c2f320adcc
Nir Assraf <nirds96@gmail.com>
2021-12-05 11:54:03 +0200

    Add files via upload

---
+++ client/src/components/game/index.tsx
@@ -0,0 +1,250 @@
+import React, { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import gameContext from "../../context/gameContext";
+import gameService from "../../services/gameService";
+import socketService from "../../services/socketService";
+import Messages from "../chat";
+
+interface ICellProps {
+  borderTop?: boolean;
+  borderRight?: boolean;
+  borderLeft?: boolean;
+  borderBottom?: boolean;
+}
+
+//Styles
+const GameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: "Arial", cursive;
+  position: relative;
+  
+`;
+const RowContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+const Cell = styled.div<ICellProps>`
+  width: 13em;
+  height: 9em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  cursor: pointer;
+  border-top: ${({ borderTop }) => borderTop && "3px solid #8e44ad"};
+  border-left: ${({ borderLeft }) => borderLeft && "3px solid #FF6F61"};
+  border-bottom: ${({ borderBottom }) => borderBottom && "3px solid #8e44ad"};
+  border-right: ${({ borderRight }) => borderRight && "3px solid #FF6F61"};
+  transition: all 270ms ease-in-out;
+
+  &:hover {
+    background-color: #8d44ad28;
+  }
+`;
+const PlayStopper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
+  cursor: default;
+`;
+const X = styled.span`
+  font-size: 100px;
+  color: yellow;
+  &::after {
+    content: "X";
+  }
+`;
+const O = styled.span`
+  font-size: 100px;
+  color: green;
+  &::after {
+    content: "O";
+  }
+`;
+const MainContainer = styled.div`
+  margin-left: 160px;
+  padding: 0px 10px;
+`;
+const SideContainer = styled.div`
+  height: 100%; /* Full-height: remove this if you want "auto" height */
+  width: 350px; /* Set the width of the sidebar */
+  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+  background-image: url("https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png");
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 20px;
+`;
+
+export type IPlayMatrix = Array<Array<string | null>>;
+export interface IStartGame {
+  start: boolean;
+  symbol: "x" | "o";
+}
+
+export function Game() {
+  const [matrix, setMatrix] = useState<IPlayMatrix>([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+
+  const {
+    playerSymbol,
+    setPlayerSymbol,
+    setPlayerTurn,
+    isPlayerTurn,
+    setGameStarted,
+    isGameStarted,
+  } = useContext(gameContext);
+
+  const checkGameState = (matrix: IPlayMatrix) => {
+    for (let i = 0; i < matrix.length; i++) {
+      let row = [];
+      for (let j = 0; j < matrix[i].length; j++) {
+        row.push(matrix[i][j]);
+      }
+
+      if (row.every((value) => value && value === playerSymbol)) {
+        return [true, false];
+      } else if (row.every((value) => value && value !== playerSymbol)) {
+        return [false, true];
+      }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+      let column = [];
+      for (let j = 0; j < matrix[i].length; j++) {
+        column.push(matrix[j][i]);
+      }
+
+      if (column.every((value) => value && value === playerSymbol)) {
+        return [true, false];
+      } else if (column.every((value) => value && value !== playerSymbol)) {
+        return [false, true];
+      }
+    }
+
+    if (matrix[1][1]) {
+      if (matrix[0][0] === matrix[1][1] && matrix[2][2] === matrix[1][1]) {
+        if (matrix[1][1] === playerSymbol) return [true, false];
+        else return [false, true];
+      }
+
+      if (matrix[2][0] === matrix[1][1] && matrix[0][2] === matrix[1][1]) {
+        if (matrix[1][1] === playerSymbol) return [true, false];
+        else return [false, true];
+      }
+    }
+
+    //Check for a tie
+    if (matrix.every((m) => m.every((v) => v !== null))) {
+      return [true, true];
+    }
+
+    return [false, false];
+  };
+
+  const updateGameMatrix = (column: number, row: number, symbol: "x" | "o") => {
+    const newMatrix = [...matrix];
+
+    if (newMatrix[row][column] === null || newMatrix[row][column] === "null") {
+      newMatrix[row][column] = symbol;
+      setMatrix(newMatrix);
+    }
+
+    if (socketService.socket) {
+      gameService.updateGame(socketService.socket, newMatrix);
+      const [currentPlayerWon, otherPlayerWon] = checkGameState(newMatrix);
+      if (currentPlayerWon && otherPlayerWon) {
+        gameService.gameWin(socketService.socket, "The Game is a TIE!");
+        alert("The Game is a TIE!");
+      } else if (currentPlayerWon && !otherPlayerWon) {
+        gameService.gameWin(socketService.socket, "You Lost!");
+        alert("You Won!");
+      }
+
+      setPlayerTurn(false);
+    }
+  };
+
+  const handleGameUpdate = () => {
+    if (socketService.socket)
+      gameService.onGameUpdate(socketService.socket, (newMatrix) => {
+        setMatrix(newMatrix);
+        checkGameState(newMatrix);
+        setPlayerTurn(true);
+      });
+  };
+
+  const handleGameStart = () => {
+    if (socketService.socket)
+      gameService.onStartGame(socketService.socket, (options) => {
+        setGameStarted(true);
+        setPlayerSymbol(options.symbol);
+        if (options.start) setPlayerTurn(true);
+        else setPlayerTurn(false);
+      });
+  };
+
+  const handleGameWin = () => {
+    if (socketService.socket)
+      gameService.onGameWin(socketService.socket, (message) => {
+        console.log("Here", message);
+        setPlayerTurn(false);
+        alert(message);
+      });
+  };
+
+  useEffect(() => {
+    handleGameUpdate();
+    handleGameStart();
+    handleGameWin();
+  }, []);
+
+  return (
+    <GameContainer>
+      <MainContainer>
+      {!isGameStarted && (
+        <h2>Waiting for Other Player to Join to Start the Game!</h2>
+      )}
+      {(!isGameStarted || !isPlayerTurn) && <PlayStopper />}
+      {matrix.map((row, rowIdx) => {
+        return (
+          <RowContainer>
+            {row.map((column, columnIdx) => (
+              <Cell
+                borderRight={columnIdx < 2}
+                borderLeft={columnIdx > 0}
+                borderBottom={rowIdx < 2}
+                borderTop={rowIdx > 0}
+                onClick={() =>
+                  updateGameMatrix(columnIdx, rowIdx, playerSymbol)
+                }
+              >
+                {column && column !== "null" ? (
+                  column === "x" ? (
+                    <X />
+                  ) : (
+                    <O />
+                  )
+                ) : null}
+              </Cell>
+            ))}
+          </RowContainer>
+        );
+      })}
+      </MainContainer>
+      <SideContainer>
+        <Messages/>
+      </SideContainer> 
+    </GameContainer>
+
+    
+  );
+}
